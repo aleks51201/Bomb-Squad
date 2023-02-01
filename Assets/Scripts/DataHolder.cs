4181ad57f3c727229df78aedf83d975f162259da
KipoGo <vit-tan@mail.ru>
2023-02-02 00:13:01 +0300

    Add

---
+++ Assets/Scripts/DataHolder.cs
@@ -0,0 +1,43 @@
+﻿namespace Assets.Scripts
+{
+    public class DataHolder
+    {
+        private int[,] array = new int[10, 10];
+
+        private void SpawnBombCoord()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+
+
+            }
+        }
+
+        private int GetRandomNum()
+        {
+            var rnd = new System.Random();
+            return rnd.Next(0, 10);
+        }
+
+        private void aslkd()
+        {
+            var arr = new int[5, 2];
+            int n = 0;
+            while (true)
+            {
+                int x= GetRandomNum();
+                int y= GetRandomNum();
+                if(arr[n,0] !=x && arr[n, 1] != y)
+                {
+
+                    n++;
+                    if (n > 4)
+                    {
+                        return ;
+                    }
+                }
+            }
+
+        }
+    }
+}
