16ec8be32e5a53205931b5242c0c740239c1cd32
KipoGo <vit-tan@mail.ru>
2023-02-02 00:37:50 +0300

    Add

--- Assets/Scripts/DataHolder.cs
+++ Assets/Scripts/DataHolder.cs
@@ -1,4 +1,6 @@
-﻿namespace Assets.Scripts
+﻿using System.Collections.Generic;
+
+namespace Assets.Scripts
 {
     public class DataHolder
     {
@@ -6,10 +8,26 @@
 
         private void SpawnBombCoord()
         {
-            for (int i = 0; i < 5; i++)
+            var arr = new List<Bomb>();
+            int n = 0;
+            Bomb bomb;
+            while (n < 5)
             {
-
-
+                bomb = new(GetRandomNum(), GetRandomNum());
+                if (arr.Count == 0)
+                {
+                    arr.Add(bomb);
+                    n++;
+                    continue;
+                }
+                foreach(var bm in arr)
+                {
+                    if (bm.IsEqualsBobm(bomb))
+                    {
+                        continue;
+                    }
+                }
+                n++;
             }
         }
 
@@ -21,22 +39,6 @@
 
         private void aslkd()
         {
-            var arr = new int[5, 2];
-            int n = 0;
-            while (true)
-            {
-                int x= GetRandomNum();
-                int y= GetRandomNum();
-                if(arr[n,0] !=x && arr[n, 1] != y)
-                {
-
-                    n++;
-                    if (n > 4)
-                    {
-                        return ;
-                    }
-                }
-            }
 
         }
     }
