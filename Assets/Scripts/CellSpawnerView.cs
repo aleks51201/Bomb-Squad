5d2bf0a15981081567aa4845ca3ce1320c16fc78
KipoGo <vit-tan@mail.ru>
2023-02-01 23:33:39 +0300

    Add feature
    Add spawn cells

--- Assets/Scripts/CellSpawnerView.cs
+++ Assets/Scripts/CellSpawnerView.cs
@@ -4,9 +4,25 @@ namespace Assets.Scripts
 {
     public class CellSpawnerView: MonoBehaviour
     {
+        [SerializeField]private CellView cellView;
+
+
         private void SpawnCell()
         {
+            Instantiate(cellView, this.transform);
+        }
 
+        private void SpawnCels()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                SpawnCell();
+            }
+        }
+
+        private void Start()
+        {
+            SpawnCels();
         }
     }
 }
