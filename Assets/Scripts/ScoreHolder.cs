78d410c70ebd0f697f803e6cf1ea84c5af10c02b
KipoGo <vit-tan@mail.ru>
2023-02-02 20:56:33 +0300

    Add

---
+++ Assets/Scripts/ScoreHolder.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Assets.Scripts
+{
+    public class ScoreHolder
+    {
+        private int _score;
+
+
+        public int Score => _score;
+
+
+        public void Init()
+        {
+            CellView.ButtonClickedEbent += AddPoint;
+        }
+
+        private void AddPoint()
+        {
+            _score +=1;
+        }
+    }
+}
