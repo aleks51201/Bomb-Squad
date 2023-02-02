c06205abe125d51d5e30681eae290e4fbcf77206
KipoGo <vit-tan@mail.ru>
2023-02-02 21:08:10 +0300

    Add

---
+++ Assets/Scripts/PanelActivator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PanelActivator:MonoBehaviour
+    {
+        [SerializeField] GameObject panel;
+
+
+        private void SetActive()
+        {
+
+        }
+
+        private void OnEnable()
+        {
+            CellView.GameLosedEvent += SetActive;
+        }
+
+        private void OnDisable()
+        {
+            CellView.GameLosedEvent -= SetActive;
+        }
+    }
+}
