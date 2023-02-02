7497481e8d2547c1c436e050695e43b897d6e029
KipoGo <vit-tan@mail.ru>
2023-02-02 20:09:39 +0300

    Add

--- Assets/Scripts/DataHolder.cs
+++ Assets/Scripts/DataHolder.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Assets.Scripts
 {
@@ -6,11 +7,12 @@ namespace Assets.Scripts
     {
         private int[,] array = new int[10, 10];
         private bool[,] array1 = new bool[10, 10];
+        private List<Bomb> arr;
 
 
         private void SpawnBombCoord()
         {
-            var arr = new List<Bomb>();
+            arr = new List<Bomb>();
             int n = 0;
             Bomb bomb;
             while (n < 5)
@@ -22,7 +24,7 @@ namespace Assets.Scripts
                     n++;
                     continue;
                 }
-                foreach(var bm in arr)
+                foreach (Bomb bm in arr)
                 {
                     if (bm.IsEqualsBobm(bomb))
                     {
@@ -31,27 +33,39 @@ namespace Assets.Scripts
                 }
                 n++;
             }
-            foreach(var bmb in arr)
+            foreach (Bomb bmb in arr)
             {
                 array1[bmb.Y, bmb.X] = true;
+                array[bmb.Y, bmb.X] = 0;
             }
         }
 
         private void lakdjf()
         {
-            var coords = new Coord[4] { new Coord(0,1)};
-
+            for(int i= 0; i < 10; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    if( array1[i, j])
+                    {
+                        continue;
+                    }
+                    foreach(var bmb in arr)
+                    {
+                        int numMax = Math.Max(bmb.X, bmb.Y);
+                        if(array[i,j] ==0 || array[i,j] > numMax)
+                        {
+                            array[i, j] = numMax;
+                        }
+                    }
+                }
+            }
         }
 
         private int GetRandomNum()
         {
-            var rnd = new System.Random();
+            System.Random rnd = new System.Random();
             return rnd.Next(0, 10);
-        }
-
-        private void aslkd()
-        {
-
         }
     }
 }
