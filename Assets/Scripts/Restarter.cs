11f6447bdfe918f15a5357ff24de1d1faeb91fe2
KipoGo <vit-tan@mail.ru>
2023-02-04 11:30:36 +0300

    Add

---
+++ Assets/Scripts/Restarter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Restarter:MonoBehaviour
+    {
+        private CellView[] GetCells()
+        {
+            return GetComponentsInChildren<CellView>();
+        }
+
+        private void Update()
+        {
+            if (!Input.GetKey(KeyCode.Space))
+            {
+                return;
+            }
+
+        }
+    }
+}
