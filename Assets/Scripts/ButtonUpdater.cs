0c7318aacd44d6fa5e6792f6ed333d62f7b753fc
KipoGo <vit-tan@mail.ru>
2023-02-11 23:21:54 +0300

    Add

---
+++ Assets/Scripts/ButtonUpdater.cs
@@ -0,0 +1,27 @@
+﻿using Scripts.Yandex;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ButtonUpdater:MonoBehaviour
+    {
+        [SerializeField] private ScoreSaver _scoreSaver;
+        [SerializeField] private Btn _btn;
+
+
+        private void OnEnable()
+        {
+            
+        }
+
+        private void OnDisable()
+        {
+            
+        }
+    }
+}
