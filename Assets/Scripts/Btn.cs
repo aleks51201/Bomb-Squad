510f6751270a131f864993d26384216d3e426eb4
KipoGo <vit-tan@mail.ru>
2023-02-11 23:09:46 +0300

    Add
    Added button

---
+++ Assets/Scripts/Btn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Btn : MonoBehaviour
+    {
+        public Action ButtonClickedEvent;
+
+
+        private void OnClick()
+        {
+            ButtonClickedEvent?.Invoke();
+        }
+    }
+}
