a2b57c8b3cbe454cbeced74e20c7c26cb8c7a036
KipoGo <vit-tan@mail.ru>
2023-02-11 23:28:41 +0300

    Fix bug

--- Assets/Scripts/ButtonUpdater.cs
+++ Assets/Scripts/ButtonUpdater.cs
@@ -6,17 +6,17 @@ namespace Assets.Scripts
     public class ButtonUpdater:MonoBehaviour
     {
         [SerializeField] private ScoreSaver _scoreSaver;
-        [SerializeField] private Btn _btn;
+        [SerializeField] private GameObject _btn;
 
 
         private void OnEnable()
         {
             if (_scoreSaver.IsPlayerAuth)
             {
-                _btn.gameObject.SetActive(false);
+                _btn.SetActive(false);
                 return;
             }
-            _btn.gameObject.SetActive(true);
+            _btn.SetActive(true);
             
         }
     }
