ad8c37cf80abae400e125e70f8d27fcf76dda4ff
KipoGo <vit-tan@mail.ru>
2023-02-15 23:59:41 +0300

    Add test

---
+++ BombSquadTest/BombCounterTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BombSquadTest
+{
+    [TestClass]
+    public class BombCounterTest
+    {
+        [TestMethod]
+        public void FiveBombInArrayCheck()
+        {
+
+        }
+    }
+}
